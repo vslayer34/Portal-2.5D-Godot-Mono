639f6bbe17e5c70ae89531696c7223a08474252e
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-06-09 12:06:20 +0300

    Created the action map for moving right, left, jumping, primary and secondary fire with the relevant helper class for those constants

--- Scripts/Characters/EmyController.cs
+++ Scripts/Characters/EmyController.cs
@@ -1,7 +1,16 @@
 using Godot;
 using System;
 
+namespace PortalD2.D.Scripts.Characters;
 public partial class EmyController : CharacterBody3D
 {
-	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 }
---
+++ Scripts/Helper/InputMapActionNames.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalD2.D.Scripts.Helper;
+/// <summary>
+/// Contains reference to input map action names
+/// </summary>
+public class InputMapActionNames
+{
+    /// <summary>
+    /// Reference to the<c>"move_right"</c>input action name
+    /// </summary>
+    public const string MOVE_RIGHT = "move_right";
+
+    /// <summary>
+    /// Reference to the<c>"move_left"</c>input action name
+    /// </summary>
+    public const string MOVE_LEFT = "move_left";
+
+    /// <summary>
+    /// Reference to the<c>"jump"</c>input action name
+    /// </summary>
+    public const string JUMP = "jump";
+
+    /// <summary>
+    /// Reference to the<c>"primary_fire"</c>input action name
+    /// </summary>
+    public const string PRIMARY_FIRE = "primary_fire";
+
+    /// <summary>
+    /// Reference to the<c>"secondary_fire"</c>input action name
+    /// </summary>
+    public const string SECONDARY_FIRE = "secondary_fire";
+}
