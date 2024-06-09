793d45d05546546f3f3c7606545b05715e1e76ff
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-06-09 16:11:00 +0300

    Added character rotation when changing direction

--- Scripts/Characters/EmyController.cs
+++ Scripts/Characters/EmyController.cs
@@ -1,7 +1,6 @@
 using Godot;
 using Portal2_5D.Scripts.Helper;
 using System;
-using System.Reflection;
 
 namespace Portal2_5D.Scripts.Characters;
 public partial class EmyController : CharacterBody3D
@@ -9,12 +8,26 @@ public partial class EmyController : CharacterBody3D
 	[Signal]
 	public delegate void OnJumpPressedEventHandler();
 
+	[Signal]
+	public delegate void OnChangeDirectionEventHandler();
+
 	enum MovementDirection
 	{
 		None = 0,
 		Right = 1,
 		Left = -1
 	}
+
+	enum HeadingDirection
+	{
+		Right,
+		Left
+	}
+
+	[ExportGroup("Required Nodes")]
+	[Export]
+	public Node3D Pivot { get; private set; }
+	[ExportGroup("")]
 
 
 	[ExportCategory("Character Properties")]
@@ -24,10 +37,13 @@ public partial class EmyController : CharacterBody3D
 	[Export]
 	private float _jumpForce = 500.0f;
 
-	private const float GRAVITY = -9.8f;
+	
+	private const float GRAVITY = -15.0f;
 
 
 	private MovementDirection _inputDirection;
+	private HeadingDirection _currentHeadingDirection = HeadingDirection.Right;
+	private HeadingDirection _lastHeadingDirection;
 	private Vector3 _velocity;
 
 
@@ -37,6 +53,7 @@ public partial class EmyController : CharacterBody3D
     public override void _Ready()
     {
 		OnJumpPressed += Jump;
+		OnChangeDirection += RotateCharacter;
     }
 
     public override void _Input(InputEvent @event)
@@ -46,11 +63,13 @@ public partial class EmyController : CharacterBody3D
         if (@Input.IsActionPressed(InputMapActionNames.MOVE_RIGHT))
 		{
 			_inputDirection = MovementDirection.Right;
+			_currentHeadingDirection = HeadingDirection.Right;
 		}
 
 		if (@Input.IsActionPressed(InputMapActionNames.MOVE_LEFT))
 		{
 			_inputDirection = MovementDirection.Left;
+			_currentHeadingDirection = HeadingDirection.Left;
 		}
 
 		if (Input.IsActionJustPressed(InputMapActionNames.JUMP))
@@ -67,6 +86,16 @@ public partial class EmyController : CharacterBody3D
 		{
 			GD.Print("secondary attack is pressed");
 		}
+
+		if (_lastHeadingDirection == _currentHeadingDirection)
+		{
+			return;
+		}
+		else
+		{
+			_lastHeadingDirection = _currentHeadingDirection;
+			EmitSignal(SignalName.OnChangeDirection);
+		}
     }
 
 
@@ -78,7 +107,7 @@ public partial class EmyController : CharacterBody3D
 		}
 		// _velocity = new Vector3(0.0f, 0.0f, (int)_inputDirection * _speed * (float)delta);
 		_velocity.Z = (int)_inputDirection * _speed * (float)delta;
-		GD.Print(_velocity);
+		// GD.Print(_velocity);
 		
 		
 		Velocity = _velocity;
@@ -96,7 +125,27 @@ public partial class EmyController : CharacterBody3D
 		}
 		_velocity.Y = _jumpForce * 1;
 		// GD.Print(_movementDirection);
-		GD.Print(Velocity);
+		// GD.Print(Velocity);
+	}
 
+
+	private async void RotateCharacter()
+	{
+		Vector3 currentRotationDegress = Pivot.RotationDegrees;
+		float targetRotationAngle = _currentHeadingDirection == HeadingDirection.Right ? 0.0f : -180.0f;
+		
+		Vector3 targetRotation = new Vector3(Pivot.Rotation.X, targetRotationAngle, Pivot.Rotation.Z);
+		float timer = 0.0f;
+		float maxTime = 0.5f;
+
+		while (timer < maxTime)
+		{
+			Pivot.RotationDegrees = currentRotationDegress.Lerp(targetRotation, timer / maxTime);
+			timer += (float)GetProcessDeltaTime();
+			
+			await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+		}
+
+		Pivot.RotationDegrees = targetRotation;
 	}
 }
