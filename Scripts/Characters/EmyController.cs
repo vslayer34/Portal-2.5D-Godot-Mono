56743ec8a72cb388616365263279258b8236b096
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-06-09 14:15:08 +0300

    Addded Jump and basic movement

--- Scripts/Characters/EmyController.cs
+++ Scripts/Characters/EmyController.cs
@@ -1,25 +1,43 @@
 using Godot;
 using Portal2_5D.Scripts.Helper;
 using System;
+using System.Reflection;
 
 namespace Portal2_5D.Scripts.Characters;
 public partial class EmyController : CharacterBody3D
 {
+	[Signal]
+	public delegate void OnJumpPressedEventHandler();
+
 	enum MovementDirection
 	{
 		None = 0,
 		Right = 1,
 		Left = -1
 	}
-	
+
 
 	[ExportCategory("Character Properties")]
 	[Export]
 	private float _speed = 10.0f;
 
+	[Export]
+	private float _jumpForce = 500.0f;
+
+	private const float GRAVITY = -9.8f;
+
 
 	private MovementDirection _inputDirection;
-	private Vector3 _movementDirection;
+	private Vector3 _velocity;
+
+
+
+    // Game Loop Methods---------------------------------------------------------------------------
+
+    public override void _Ready()
+    {
+		OnJumpPressed += Jump;
+    }
 
     public override void _Input(InputEvent @event)
     {
@@ -37,7 +55,7 @@ public partial class EmyController : CharacterBody3D
 
 		if (Input.IsActionJustPressed(InputMapActionNames.JUMP))
 		{
-			GD.Print("jump is pressed");
+			EmitSignal(SignalName.OnJumpPressed);
 		}
 
 		if (Input.IsActionJustPressed(InputMapActionNames.PRIMARY_FIRE))
@@ -54,9 +72,31 @@ public partial class EmyController : CharacterBody3D
 
     public override void _PhysicsProcess(double delta)
     {
-		_movementDirection = new Vector3(0.0f, Velocity.Y, (int)_inputDirection * _speed * (float)delta);
-		Velocity = _movementDirection;
+		if (!IsOnFloor())
+		{
+			_velocity.Y += GRAVITY * (float)delta;
+		}
+		// _velocity = new Vector3(0.0f, 0.0f, (int)_inputDirection * _speed * (float)delta);
+		_velocity.Z = (int)_inputDirection * _speed * (float)delta;
+		GD.Print(_velocity);
+		
+		
+		Velocity = _velocity;
+		
 
         MoveAndSlide();
     }
+
+
+	private void Jump()
+	{
+		if (!IsOnFloor())
+		{
+			return;
+		}
+		_velocity.Y = _jumpForce * 1;
+		// GD.Print(_movementDirection);
+		GD.Print(Velocity);
+
+	}
 }
