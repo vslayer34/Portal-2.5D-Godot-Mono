985508d5925797ee2e1f44899350b00982678c0f
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-06-23 04:13:36 +0300

    scene updates

--- Scripts/Objects/PortalGun.cs
+++ Scripts/Objects/PortalGun.cs
@@ -3,13 +3,18 @@ using System;
 
 public partial class PortalGun : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+	[ExportGroup("Required Nodes")]	
+	[Export]
+	public Node3D LaunchPoint { get; private set; }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+
+
+	// Game Loop Methods---------------------------------------------------------------------------
+
+	// Mebmer Methods------------------------------------------------------------------------------
+
+	public void ShootPortal()
 	{
+		// RayCastOrigin.
 	}
 }
